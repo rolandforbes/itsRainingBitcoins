02b95848a0b450a68a65e8004b68ab514309505b
Roland Forbes <rolandandrewforbes@gmail.com>
2019-05-24 23:49:31 -0600

    Initial Commit

---
+++ itsRainingBitcoins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  itsRainingBitcoins
+//
+//  Created by Roland Forbes on 5/24/19.
+//  Copyright © 2019 Roland Forbes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
